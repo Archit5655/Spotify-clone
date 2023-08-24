fd57c3fafaf10292021fe850b1cc3852b0b7d027
Archit <architgargagra@gmail.com>
2023-08-24 22:56:02 +0530

    seted up db

---
+++ providers/Supabaseprovider.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { Database } from "@/types_db"
+import React from "react"
+
+interface supabaseproviderprops{
+    children:React.ReactNode,
+}
+const supabaseprovider:React.FC<supabaseproviderprops>=({children})=>{
+    {children}
+    
+
+}
